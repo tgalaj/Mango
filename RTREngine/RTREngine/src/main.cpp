214e6b2ce40b0a2f94b6093b6a72834d28d1ec7c
Tomasz Gałaj <tomaszgalaj@gmail.com>
2015-06-29 22:11:00 +0200

    Added SDL support - init version - need to config.

--- RTREngine/RTREngine/src/main.cpp
+++ RTREngine/RTREngine/src/main.cpp
@@ -1,6 +1,8 @@
 #include <GL\glew.h>
-#include <GLFW\glfw3.h>
 #include <GUI\AntTweakBar.h>
+#include <SDL2\SDL.h>
+#include <SDL2\SDL_opengl.h>
+#include <gl\GLU.h>
 
 #include <cstdio>
 #include <cstdlib>
@@ -10,84 +12,100 @@
 #define GL_VERSION_MAJOR 4
 #define GL_VERSION_MINOR 5
 
-static void mouseButtonCB(GLFWwindow* window, int button, int action, int mods)
+//static void mouseButtonCB(GLFWwindow* window, int button, int action, int mods)
+//{
+//	if (!TwEventMouseButtonGLFW(button, action))
+//	{
+//	}
+//}
+//
+//static void mousePosCB(GLFWwindow* window, double mouseX, double mouseY)
+//{
+//	if (!TwEventMousePosGLFW(static_cast<int>(mouseX), static_cast<int>(mouseY)))
+//	{ 
+//	}
+//}
+//
+//static void mouseScrollCB(GLFWwindow* window, double xoffset, double yoffset)
+//{
+//	if (!TwEventMouseWheelGLFW(yoffset))
+//	{
+//	}
+//}
+//
+//static void keyCB(GLFWwindow* window, int key, int scancode, int action, int mods)
+//{
+//	if (!TwEventCharGLFW(key, action))
+//	{
+//		if (key == GLFW_KEY_ESCAPE)
+//		{
+//			glfwDestroyWindow(window);
+//		}
+//
+//	}
+//}
+//
+//static void onResize(GLFWwindow* window, int w, int h)
+//{
+//	if (h == 0)
+//		h = 1;
+//
+//	glViewport(0, 0, w, h);
+//
+//	//update proj matrix
+//	float ascpetRatio = static_cast<float>(w) / static_cast<float>(h);
+//
+//	TwWindowSize(w, h);
+//}
+
+int main(int argc, char* args[])
 {
-	if (!TwEventMouseButtonGLFW(button, action))
-	{
-	}
-}
-
-static void mousePosCB(GLFWwindow* window, double mouseX, double mouseY)
-{
-	if (!TwEventMousePosGLFW(static_cast<int>(mouseX), static_cast<int>(mouseY)))
-	{ 
-	}
-}
-
-static void mouseScrollCB(GLFWwindow* window, double xoffset, double yoffset)
-{
-	if (!TwEventMouseWheelGLFW(yoffset))
-	{
-	}
-}
-
-static void keyCB(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-	if (!TwEventCharGLFW(key, action))
-	{
-		if (key == GLFW_KEY_ESCAPE)
-		{
-			glfwDestroyWindow(window);
-		}
-
-	}
-}
-
-static void onResize(GLFWwindow* window, int w, int h)
-{
-	if (h == 0)
-		h = 1;
-
-	glViewport(0, 0, w, h);
-
-	//update proj matrix
-	float ascpetRatio = static_cast<float>(w) / static_cast<float>(h);
-
-	TwWindowSize(w, h);
-}
-
-int main(void)
-{
-	GLFWwindow* window = nullptr;
+	SDL_Window*  window        = nullptr;
+    SDL_Surface* screenSurface = nullptr;
 	
-	/* Initialize the library */
-	if (!glfwInit())
+	/* Initialize the SDL library */
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		fprintf(stderr, "GLFW init failed!\n");
+		fprintf(stderr, "SDL could not initialize! SDL error :%s\n", SDL_GetError());
+        system("pause");
 		return -1;
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_VERSION_MAJOR);
+	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_VERSION_MAJOR);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_VERSION_MINOR);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);*/
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Hello World", NULL, NULL);
+    window = SDL_CreateWindow("Hello SDL!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
 	if (!window)
 	{
-		std::string glVersion = std::to_string(GL_VERSION_MAJOR) + "." + std::to_string(GL_VERSION_MINOR);
-		fprintf(stderr, "Window creation failed! OpenGL %s not supported!\n", glVersion.c_str());
+        fprintf(stderr, "Window could not be created! SDL error :%s\n", SDL_GetError());
+
+		/*std::string glVersion = std::to_string(GL_VERSION_MAJOR) + "." + std::to_string(GL_VERSION_MINOR);
+		fprintf(stderr, "Window creation failed! OpenGL %s not supported!\n", glVersion.c_str());*/
 		system("pause");
 		return -1;
 	}
 
+    /* Get window surface */
+    screenSurface = SDL_GetWindowSurface(window);
+
+    //Fill the surface white
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+    //Update the surface
+    SDL_UpdateWindowSurface(window);
+
+    //Wait two seconds
+    SDL_Delay(2000);
+
 	/* Set input mode */
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	//glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
 	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
+	//glfwMakeContextCurrent(window);
 
 	glewExperimental = GL_TRUE;
 
@@ -95,7 +113,7 @@ int main(void)
 	if (err != GLEW_OK)
 	{
 		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-		glfwTerminate();
+		//glfwTerminate();
 		system("pause");
 		return -1;
 	}
@@ -117,34 +135,38 @@ int main(void)
 	int my_var = 10;
 	TwAddVarRW(myBar, "Var name", TW_TYPE_INT32, &my_var, "");
 
-	glfwSetMouseButtonCallback(window, (GLFWmousebuttonfun)mouseButtonCB);
+	/*glfwSetMouseButtonCallback(window, (GLFWmousebuttonfun)mouseButtonCB);
 	glfwSetCursorPosCallback(window, (GLFWcursorposfun)mousePosCB);
 	glfwSetScrollCallback(window, (GLFWscrollfun)mouseScrollCB);
 	glfwSetKeyCallback(window, (GLFWkeyfun)keyCB);
-	glfwSetFramebufferSizeCallback(window, (GLFWframebuffersizefun)onResize);
+	glfwSetFramebufferSizeCallback(window, (GLFWframebuffersizefun)onResize);*/
 	
 	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		glClear(GL_COLOR_BUFFER_BIT);
+	//while (!glfwWindowShouldClose(window))
+	//{
+	//	glClear(GL_COLOR_BUFFER_BIT);
 
-		TwDraw();
+	//	TwDraw();
 
-		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-			printf("Guzik W!\n");
+	//	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	//		printf("Guzik W!\n");
 
-		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-			printf("Guzik A!\n");
+	//	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	//		printf("Guzik A!\n");
 
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
+	//	/* Swap front and back buffers */
+	//	glfwSwapBuffers(window);
 
-		/* Poll for and process events */
-		glfwPollEvents();
-	}
+	//	/* Poll for and process events */
+	//	glfwPollEvents();
+	//}
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+    //Destroy window
+    SDL_DestroyWindow(window);
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
 	TwTerminate();
 	return 0;
 }
