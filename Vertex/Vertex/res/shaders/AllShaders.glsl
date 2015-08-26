bc49ed3312eb229e1a570d1e89f584b397ce3d42
Shot511 <tomaszgalaj@gmail.com>
2015-08-26 15:41:25 +0200

    Added basic version of a spot light.
    Lightning needs serious improvement in the future.

--- Vertex/Vertex/res/shaders/AllShaders.glsl
+++ Vertex/Vertex/res/shaders/AllShaders.glsl
@@ -28,12 +28,20 @@ in vec2 o_texcoord;
 
 out vec4 fragColor;
 
+//Dir light properties
 vec4 lightPos       = vec4(0.0f, 10.0f, -10.0f, 1.0f);
 vec3 lightIntensity = vec3(1.0f);
 vec3 Kd             = vec3(1.0f);
 vec3 Ka             = vec3(1.0f);
 vec3 Ks             = vec3(1.0f);
 float shininess     = 20.0f;
+
+//Spot light properties
+vec3 spotPosition  = vec3(5.0f, 3.0f, 4.0f);
+vec3 spotIntensity = vec3(1.0f);
+vec3 spotDir       = vec3(0.0f) - spotPosition;
+float spotExponent = 0.5f;
+float cutoff       = 20.0f; //degrees
 
 vec3 ads()
 {
@@ -45,7 +53,31 @@ vec3 ads()
 	return lightIntensity * (Ka + Kd * max(dot(s, n), 0.0f) + Ks * pow(max(dot(h, n), 0.0f), shininess));
 }
 
+vec3 spotlightAds()
+{
+	spotDir = normalize(spotDir);
+
+	vec3 s = normalize(spotPosition - o_position);
+	float angle = acos(dot(-s, spotDir));
+	cutoff = radians(clamp(cutoff, 0.0f, 90.0f));
+	vec3 ambient = spotIntensity * Ka * vec3(0.1f, 0.1f, 0.1f);
+
+	if(angle < cutoff)
+	{
+		float spotFactor = pow(dot(-s, spotDir), spotExponent);
+
+		vec3 v = normalize(-o_position);
+		vec3 h = normalize(v + s);
+
+		return ambient + spotFactor * spotIntensity * (Kd * max(dot(s, o_normal), 0.0f) + Ks * pow(max(dot(h, o_normal), 0.0f), shininess));
+	}
+	else
+	{
+		return ambient;
+	}
+}
+
 void main()
 {      
-	fragColor = vec4(ads() * vec3(0.9, 0.2, 0.1), 1.0f);
+	fragColor = vec4(spotlightAds() * vec3(0.9, 0.2, 0.1), 1.0f);
 }
