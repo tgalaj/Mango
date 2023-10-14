e97fe449b76330af0b681ae356302ff5e19ef515
Tomasz Gałaj <tomaszgalaj@gmail.com>
2023-10-14 15:17:49 +0200

    Clean up cmake config

--- cmake/global_settings.cmake
+++ cmake/global_settings.cmake
@@ -12,6 +12,18 @@ set(GLFW_BUILD_TESTS    OFF CACHE INTERNAL "Build the GLFW test programs")
 set(GLFW_BUILD_DOCS     OFF CACHE INTERNAL "Build the GLFW documentation")
 set(GLFW_INSTALL        OFF CACHE INTERNAL "Generate installation target")
 
-set(BUILD_SHARED_LIBS         OFF CACHE INTERNAL "Build package with shared libraries.")
-set(ASSIMP_BUILD_ASSIMP_TOOLS OFF CACHE INTERNAL "If the supplementary tools for Assimp are built in addition to the library.")
-set(ASSIMP_BUILD_TESTS        OFF CACHE INTERNAL "If the test suite for Assimp is built in addition to the library.")
+# Set Assimp variables
+set(BUILD_SHARED_LIBS                     OFF CACHE INTERNAL "Build package with shared libraries.")
+set(ASSIMP_NO_EXPORT                      ON  CACHE BOOL "")
+set(ASSIMP_BUILD_ASSIMP_TOOLS             OFF CACHE BOOL "")
+set(ASSIMP_BUILD_TESTS                    OFF CACHE BOOL "")
+set(ASSIMP_INSTALL_PDB                    OFF CACHE BOOL "")
+set(ASSIMP_BUILD_ALL_IMPORTERS_BY_DEFAULT OFF CACHE BOOL "")
+set(ASSIMP_BUILD_OBJ_IMPORTER             ON  CACHE BOOL "")
+set(ASSIMP_BUILD_GLTF_IMPORTER            ON  CACHE BOOL "")
+set(ASSIMP_BUILD_FBX_IMPORTER             ON  CACHE BOOL "")
+set(ASSIMP_BUILD_MD5_IMPORTER             ON  CACHE BOOL "")
+
+# Set entityX variables
+set(ENTITYX_BUILD_SHARED  FALSE CACHE INTERNAL "Build shared libraries?")
+set(ENTITYX_BUILD_TESTING FALSE CACHE INTERNAL "Enable building of tests.")
